1112b03403e282b961d67cf4dc38a3b669322dc6
ergang2000 <erhan-kurnaz@hotmail.com>
2018-11-14 09:22:35 +0100

    hi again

--- src/AppBundle/Entity/Medicijn.php
+++ src/AppBundle/Entity/Medicijn.php
@@ -8,8 +8,30 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="medicijnen")
+ */
 class Medicijn
 {
-
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $naam;
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $werking;
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $bijwerking;
 }
